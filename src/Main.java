b64386a270724c7dfa96d7fe574c70d716ac598c
Dimasik <dimka14493@gmail.com>
2023-03-04 12:10:50 +0500

    first commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketPrice = 13_678;
+        int rubMiles = 20;
+        int bonusMiles = ticketPrice / rubMiles;
+        System.out.println(bonusMiles);
+
+
+    }
+}
